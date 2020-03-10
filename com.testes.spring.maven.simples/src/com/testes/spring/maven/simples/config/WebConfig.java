775d3f8426c330ba075ced8af2f35a3e8cf78ece
gdonega <gustavo.dq.27@hotmail.com>
2020-03-09 21:18:12 -0300

    init

---
+++ com.testes.spring.maven.simples/src/com/testes/spring/maven/simples/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.testes.spring.maven.simples.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.testes.spring.maven.simples")
+public class WebConfig {
+
+}
